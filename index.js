cea4b92c8fc98f1d436e68263596402756f4367a
root <root@localhost.localdomain>
2018-05-28 23:25:17 +0800

    hot fix

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const scheduleTask = require('./src/scheduleTask');
-
+const http = require('http');
 const runApp = () => {
     scheduleTask();
+    http.createServer((req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end('hellO world!')
+    }).listen(3000);
+    console.log('server running on http://118.24.119.37');
 }
 
 runApp();
--- src/scheduleTask.js
+++ src/scheduleTask.js
@@ -1,8 +1,8 @@
 const schedule = require('node-schedule');
 const saveScreenshot = require('./saveScreenshot');
 const sendEmail = require('./sendEmail');
-const scheduleTask = async () => {
-    schedule.scheduleJob('0 * * * * *', () => {
+const scheduleTask = () => {
+    schedule.scheduleJob('0 0 * * * *', async () => {
         await saveScreenshot('http://baidu.com', './src/google.png');
         sendEmail();
         console.log('scheduleCronstyle:' + new Date());
