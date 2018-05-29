dd0564ac1ed0520bc1f8dca5bbfb2b19ed8456a6
xpc徐鹏春 <xu_pc@ctrip.com>
2018-05-29 16:32:26 +0800

    实现登陆推特的逻辑

--- src/saveScreenshot.js
+++ src/saveScreenshot.js
@@ -7,16 +7,29 @@ const puppeteer = require('puppeteer');
 const saveScreenshot = async (url, path) => {
 
     // 启动浏览器
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox',  '--proxy-server=127.0.0.1:1235' ]});
+    const browser = await puppeteer.launch({headless: false, args: [ '--disable-setuid-sandbox',  '--proxy-server=127.0.0.1:1235' ]});
     // 打开页面
     const page = await browser.newPage();
     // 设置浏览器视窗
     page.setViewport({
         width: 1376,
         height: 768,
-    });
+    }); 
     // 地址栏输入网页地址
     await page.goto(url);
+    await page.waitForSelector('.js-username-field')
+    await page.type('.js-username-field', '635157329@qq.com', {delay: 100});
+    await page.type('.js-password-field', 'imissyou.123', {delay: 100});
+    
+    await page.click('button.submit')
+    await page.waitForNavigation({ waitUntil: 'load' });
+    if (page.url() === 'https://twitter.com/'){
+        
+    } else {
+        await page.type('#challenge_response', '635157329@qq.com', {delay: 100});
+        await page.click('#email_challenge_submit');
+        await page.waitForNavigation({ waitUntil: 'load' });
+    }
     // 截图: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagescreenshotoptions
     await page.screenshot({ path });
     // 关闭浏览器
--- src/scheduleTask.js
+++ src/scheduleTask.js
@@ -1,9 +1,9 @@
 const schedule = require('node-schedule');
 const saveScreenshot = require('./saveScreenshot');
 const sendEmail = require('./sendEmail');
-const scheduleTask = () => {
-    schedule.scheduleJob('0 * * * * *', async () => {
-        await saveScreenshot('https://google.com', './src/google.png');
+const scheduleTask = async() => {
+    schedule.scheduleJob('0 0 * * * *', async () => {
+        await saveScreenshot('https://twitter.com/login', './src/google.png');
         sendEmail();
         console.log('scheduleCronstyle:' + new Date());
     });
