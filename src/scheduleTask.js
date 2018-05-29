7334f181bb50739d921ed59154289bac1ec9bea6
xpc徐鹏春 <xu_pc@ctrip.com>
2018-05-29 14:08:29 +0800

    定时时间更改

--- src/scheduleTask.js
+++ src/scheduleTask.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const saveScreenshot = require('./saveScreenshot');
 const sendEmail = require('./sendEmail');
 const scheduleTask = () => {
-    schedule.scheduleJob('0 0 * * * *', async () => {
+    schedule.scheduleJob('0 0 16 * * *', async () => {
         await saveScreenshot('https://studiofow.com', './src/google.png');
         sendEmail();
         console.log('scheduleCronstyle:' + new Date());
